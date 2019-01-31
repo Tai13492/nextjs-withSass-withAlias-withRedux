f5a8bf006c18a3f20e7cd6bf8768b0d67a94749e
Tai13492 <friendship045@gmail.com>
2019-01-31 22:02:55 +0700

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import React from "react";
+import App, { Container } from "next/app";
+import "../sass/index.scss";
+
+class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Test from "components/Test";
+import React from "react";
+export default () => (
+  <React.Fragment>
+    <div className="test">Welcome to next.js !</div>
+    <Test />
+  </React.Fragment>
+);
